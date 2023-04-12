c73489d7418d3a22e4bfd490b5ad99d60313211a
zino1187 <zino1187@gmail.com>
2023-04-12 12:25:38 +0900

    초기 커밋

---
+++ src/main/java/com/edu/b2bshop/shop/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.edu.b2bshop.shop.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	private Logger logger=LoggerFactory.getLogger(this.getClass());
+	
+	@GetMapping("/")
+	public ModelAndView getMain(HttpServletRequest request) {
+		logger.info("메인호출함");
+		ModelAndView mav = new ModelAndView("shop/index");
+		
+		return mav;
+	}
+}
